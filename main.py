19978322e843ae3bae279668afff80663133b384
chancedom95 <CHANCE.PENA@YAHOO.COM>
2026-06-16 17:48:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def lbank_headers():
     return {
         "Content-Type":     "application/x-www-form-urlencoded",
         "timestamp":         lbank_ts(),
-        "signature_method": "HmacSHA256",
+        "signature_method": "HMACSHA256",
         "echostr":           lbank_echostr(),
     }
 
